63dcf552330511a9a555a88b6fb2ab3830b515fd
bharghava52 <bhargavaraja12@gmail.com>
2017-12-28 10:27:51 +0530

    Update ConnectionHandling.java
    implemented using singleton class

--- src/connectionhandling/ConnectionHandling.java
+++ src/connectionhandling/ConnectionHandling.java
@@ -8,26 +8,24 @@ import java.util.Scanner;
 
 
 public class ConnectionHandling {
-
-    
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         int n;
         System.out.println("enter the number of connections to pool");
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
-        ConnectionPooling cp=new ConnectionPooling("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/test1", "root","root",n);
+        ConnectionPooling cp=ConnectionPooling.getInstance("com.mysql.jdbc.Driver","jdbc:mysql://localhost:3306/test1", "root","root",n);
         Connection con=cp.getConnection();
-        PreparedStatement prepStmt = con.prepareStatement("select * from users");
-         
+        PreparedStatement prepStmt = con.prepareStatement("select * from users"); 
+        try{
         ResultSet rs = prepStmt.executeQuery();
         while (rs.next()) {
-               System.out.print(rs.getString("name") + " ");
+               System.out.print(rs.getString("kname") + " ");
                System.out.println(rs.getString("email"));
         }
+        }catch(Exception e){
+         System.out.println("Exception:"+e);
+        }
         cp.free(con);
-        
-        
-           
+        System.out.println("exception handled");           
     }
-    
 }
